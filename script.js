e4e211658134c24b3d53b97e134fa012f179a0e5
angeltorru <133137871+angeltorru@users.noreply.github.com>
2023-05-10 18:36:32 +0200

    gpt

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Ejemplo de código JavaScript
+console.log('¡Hola, mundo!');
+
+// Ejemplo de función para manipular elementos HTML
+function cambiarColor() {
+  var elemento = document.getElementById('miParrafo');
+  elemento.style.color = 'red';
+}
